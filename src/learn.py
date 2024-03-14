7c143477a101cdfb49e8923e7fed8f745f291bb3
Mads Risager <madssr2@gmail.com>
2024-03-14 22:45:56 +0100

    Use CombinedReward as reward function

--- src/learn.py
+++ src/learn.py
@@ -2,11 +2,11 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import VecNormalize
 from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
 from stable_baselines3 import PPO
-import fastfiz_env
 from stable_baselines3.ppo.policies import MlpPolicy
-import gymnasium as gym
+import fastfiz_env
+from fastfiz_env.utils.reward_functions.common import *
+from fastfiz_env.utils.reward_functions import CombinedReward
 import os
-
 
 # Get next version
 if os.path.exists("models/"):
@@ -26,8 +26,15 @@ MODEL_DIR = f"models/{MODEL_NAME}/"
 BEST_MODEL_DIR = f"models/{MODEL_NAME}/best/"
 
 
+rewards = [StepPocketedReward(), BestTotalDistanceReward(), GameWonReward(
+), ImpossibleShotReward(), CueBallNotMovedReward(), CueBallPocketedReward(), ConstantReward()]
+weights = [5, 1, 100, -100, -100, -100, -1]
+
+reward_fn = CombinedReward(rewards, weights)
+
+
 def make_env():
-    return gym.make("BaseRLFastFiz-v0", num_balls=BALLS, max_episode_steps=100)
+    return fastfiz_env.make("BaseRLFastFiz-v0", reward_function=reward_fn, num_balls=BALLS, max_episode_steps=100)
 
 
 net_arch = dict(pi=[32, 32], vf=[32, 32])
@@ -35,12 +42,14 @@ net_arch = dict(pi=[32, 32], vf=[32, 32])
 env = VecNormalize(make_vec_env(make_env, n_envs=4),
                    training=True, norm_obs=True, norm_reward=True)
 
-model = PPO(MlpPolicy, env,
-            verbose=1, tensorboard_log=TB_LOGS_DIR, policy_kwargs={"net_arch": net_arch})
+model = PPO(MlpPolicy, env, learning_rate=0.0001,
+            verbose=1, tensorboard_log=TB_LOGS_DIR,
+            policy_kwargs={"net_arch": net_arch},
+            use_sde=True)
 
 
 checkpoint_callback = CheckpointCallback(name_prefix=MODEL_NAME,
-                                         save_freq=50_000, save_path=MODEL_DIR, save_replay_buffer=True, save_vecnormalize=True)
+                                         save_freq=50_000, save_path=MODEL_DIR, save_vecnormalize=True)
 
 
 eval_callback = EvalCallback(eval_env=env, best_model_save_path=BEST_MODEL_DIR, n_eval_episodes=10,

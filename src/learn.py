f835dbdc6f6d3a7350d35988801017186bd19a06
Mads Risager <madssr2@gmail.com>
2024-03-20 15:50:06 +0100

    New params

--- src/learn.py
+++ src/learn.py
@@ -15,20 +15,20 @@ from torch import nn as nn
 
 params = {
     "n_steps": 2048,
-    "batch_size": 16,
+    "batch_size": 64,
     "gamma": 0.9999,
-    "learning_rate": 0.000107604121853,
-    "ent_coef": 0.000883622,
-    "clip_range": 0.4,
-    "n_epochs": 5,
-    "gae_lambda": 1.0,
-    "max_grad_norm": 0.7,
-    "vf_coef": 0.5722885038,
-    "sde_sample_freq": 32,
+    "learning_rate": 0.0007350588182212963,
+    "ent_coef": 0.00014455517747388256,
+    "clip_range": 0.2,
+    "n_epochs": 1,
+    "gae_lambda": 0.92,
+    "max_grad_norm": 0.3,
+    "vf_coef": 0.9179505500616013,
+    "sde_sample_freq": 256,
     "policy_kwargs": dict(
-        log_std_init=-2.0664120001,
-        net_arch=dict(pi=[256, 256], vf=[256, 256]),
-        activation_fn=nn.ReLU,
+        log_std_init=0.6144189947543124,
+        net_arch=dict(pi=[64, 64], vf=[64, 64]),
+        activation_fn=nn.Tanh,
         ortho_init=False,
     ),
 }
@@ -48,7 +48,7 @@ else:
 
 BALLS = 2
 
-MODEL_NAME = f"ppo-v{VERSION}-b{BALLS}"
+MODEL_NAME = f"ppo-v{VERSION}-b{BALLS}-vel-norm"
 TB_LOGS_DIR = "logs/tb_logs/"
 LOGS_DIR = f"logs/{MODEL_NAME}"
 MODEL_DIR = f"models/{MODEL_NAME}/"
@@ -72,7 +72,7 @@ reward_function = CombinedReward(rewards_functions, reward_weights, short_circui
 
 def make_env():
     return fastfiz_env.make(
-        "SequenceFastFiz-v0",
+        "VelocityFastFiz-v0",
         reward_function=DefaultReward,
         num_balls=BALLS,
         max_episode_steps=100,
@@ -81,7 +81,7 @@ def make_env():
 
 
 env = VecNormalize(
-    make_vec_env(make_env, n_envs=4), training=True, norm_obs=True, norm_reward=True
+    make_vec_env(make_env, n_envs=2), training=True, norm_obs=True, norm_reward=True
 )
 
 # env = make_env()

c0909346fdfec09f70262b0a1ba4d64cc029d310
Mads Risager <madssr2@gmail.com>
2024-05-13 15:33:57 +0200

    Remove verbose

--- src/fastfiz_env/envs/pockets_fastfiz.py
+++ src/fastfiz_env/envs/pockets_fastfiz.py
@@ -80,7 +80,7 @@ class PocketsFastFiz(gym.Env):
         shot_params = ff.ShotParams(*action)
 
         if self._possible_shot(shot_params):
-            self.table_state.executeShot(shot_params, verbose=True)
+            self.table_state.executeShot(shot_params)
 
         observation = self._get_observation()
         reward = self.reward.get_reward(prev_table_state, self.table_state, action)

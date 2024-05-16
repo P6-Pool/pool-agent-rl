87e4cad72685861060c5c8601422c952add0ef04
Mads Risager <madssr2@gmail.com>
2024-05-16 15:37:44 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 import re
 from setuptools import setup
 
-# with open("requirements.txt") as f:
-#     requirements = f.read().splitlines()
-
-
 def get_version():
-    with open("src/fastfiz_env/__init__.py", "r") as f:
+    with open("fastfiz_env/__init__.py", "r") as f:
         for line in f:
             match = re.match(r"__version__\s*=\s*['\"]([^'\"]+)['\"]", line)
             if match:
@@ -16,8 +12,4 @@ def get_version():
 
 setup(
     version=get_version(),
-    # install_requires=requirements,
-    # test_requires=["pytest"],
-    # packages=find_packages(where="src"),
-    # package_dir={"": "src"},
 )
